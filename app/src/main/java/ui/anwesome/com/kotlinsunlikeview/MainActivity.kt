310484c3654a469d31e90c4da4e3bb9eed1fa9f9
Anwesh43 <anweshthecool0@gmail.com>
2018-02-24 11:53:00 +0530

    adding SunLikeListener in Activity

--- app/src/main/java/ui/anwesome/com/kotlinsunlikeview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinsunlikeview/MainActivity.kt
@@ -3,14 +3,20 @@ package ui.anwesome.com.kotlinsunlikeview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import ui.anwesome.com.sunlikeview.SunLikeView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SunLikeView.create(this)
+        var view = SunLikeView.create(this)
         fullScreen()
+        view.addSunLikeListener({
+            Toast.makeText(this, "sun has risen", Toast.LENGTH_SHORT).show()
+        }, {
+            Toast.makeText(this, "sun has set", Toast.LENGTH_SHORT).show()
+        })
     }
 }
 fun AppCompatActivity.fullScreen() {
