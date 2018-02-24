efdb0c63c75ea6d0cca897f3f1a4f8aae917c6c1
Anwesh43 <anweshthecool0@gmail.com>
2018-02-24 10:39:05 +0530

    adding SunLikeView class

--- sunlikeview/src/main/java/ui/anwesome/com/sunlikeview/SunLikeView.kt
+++ sunlikeview/src/main/java/ui/anwesome/com/sunlikeview/SunLikeView.kt
@@ -6,3 +6,17 @@ package ui.anwesome.com.sunlikeview
 import android.view.*
 import android.content.*
 import android.graphics.*
+class SunLikeView(ctx : Context) : View(ctx) {
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
