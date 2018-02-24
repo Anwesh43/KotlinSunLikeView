2004f6e225ea86f863938df63071747b625ae913
Anwesh43 <anweshthecool0@gmail.com>
2018-02-24 11:44:57 +0530

    adding fullScreen support

--- app/src/main/java/ui/anwesome/com/kotlinsunlikeview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinsunlikeview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlinsunlikeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.sunlikeview.SunLikeView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SunLikeView.create(this)
+        fullScreen()
     }
+}
+fun AppCompatActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
